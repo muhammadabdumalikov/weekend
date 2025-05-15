3f83e314848b1562b607d8e338d9f1801aa85373
muhammadabdumalikov <burakali424@gmail.com>
2025-05-15 16:02:38 +0500

    check fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require("./next-i18next.config");
+import { i18n } from "./next-i18next.config";
 const nextConfig = {
   reactStrictMode: false,
   i18n,
