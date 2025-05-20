1a7aede1207d3a57866c28e7553800edd4e129cf
muhammadabdumalikov <burakali424@gmail.com>
2025-05-20 16:18:01 +0500

    change to https

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
+        protocol: "https",
         hostname: "116.202.26.85",
         port: "9000", // Add port if required
         pathname: "/travelapp/**", // Adjust the path to match your files
@@ -17,7 +17,7 @@ const nextConfig = {
     return [
       {
         source: "/api/tour/list",
-        destination: "http://116.202.26.85:9000/api/tour/list", // Proxies API requests
+        destination: "https://api.trippo.live/api/tour/list", // Proxies API requests
       },
     ];
   },
